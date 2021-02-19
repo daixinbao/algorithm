d5f995c7136082f70b4f0aefa40bd55cb691b6ab
daixinbao <1065808217@qq.com>
2021-02-19 09:53:34 +0800

    asdsa

--- src/main/java/com/february/Day01.java
+++ src/main/java/com/february/Day01.java
@@ -10,7 +10,7 @@ import java.util.Set;
  */
 public class Day01 {
     public static void main(String[] args) {
-        Integer I=10000;
+        Integer I=10000; //saa
         Integer i=10000;
         System.out.println(I.equals(i));
     }
