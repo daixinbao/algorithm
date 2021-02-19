91b5e2b3debc630cca8b7b30891b2e5949a5e62b
daixinbao <1065808217@qq.com>
2021-02-19 09:46:36 +0800

    Initial commit

---
+++ src/main/java/com/ListNode.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by daixn on 2021/2/18 23:29
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
---
+++ src/main/java/com/february/Day01.java
@@ -0,0 +1,45 @@
+package com.february;
+
+import com.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by daixn on 2021/2/18 23:25
+ */
+public class Day01 {
+    public static void main(String[] args) {
+        Integer I=10000;
+        Integer i=10000;
+        System.out.println(I.equals(i));
+    }
+
+    /**
+     * 删除链表重复元素
+     * @param head
+     * @return
+     */
+    public ListNode removeDuplicateNodes(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode node=head;
+        Set<Integer> set=new HashSet<Integer>();
+        set.add(head.val);
+        while(node!=null ){
+            if(node.next!=null&&set.contains(node.next.val)){
+                node.next=node.next.next;
+            }else{
+                node=node.next;
+                if(node!=null){
+                    set.add(node.val);
+                }
+            }
+        }
+        return head;
+    }
+}
+
+
+
